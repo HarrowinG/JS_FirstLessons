69b36b37e03aaef97d2cea990bfecfc3f27efda3
HarrowinG <vladislavtetyuev@gmail.com>
2020-03-05 16:24:21 +0300

    lesson 5

---
+++ app.js
@@ -0,0 +1,18 @@
+//value types
+console.log(2019, 1.5);
+console.log(2019 * 'asd'); //NaN
+console.log(1 / 0); //Infinity
+
+console.log('Hello', "Hello", `Hello`);
+
+console.log(true, false);
+
+console.log(null);
+
+console.log(undefined);  //empty variable
+
+console.log(Symbol());
+
+//reference types
+console.log({ name: 'Me', age: 30 });
+console.log([1, 2, 3, 4]);
