74923661aec1fe002ca3a2fb6476491ab497ce95
Vladislav Tetyuev <vladislavtetyuev@gmail.com>
2020-04-08 20:04:37 +0300

    some more tasks for arrays

--- lesson17_arrayMethods.js
+++ lesson17_arrayMethods.js
@@ -294,3 +294,79 @@ let testArray = ["chrishna", "chrishna", "hare", "hare", "hare", "hare", "chrish
 
 console.log(testArray);
 console.log(unique(testArray));
+
+console.log("course exercises ===========");
+//1. get num and return numbers array from 1 to num
+function getArray(num) {
+  let arr = [];
+  for (let i = 1; i <= num; i++) {
+    arr.push(i);
+  }
+
+  return arr;
+}
+
+console.log(getArray(10));
+
+//2. double initial array
+function doubleArray(arr) {
+  return arr.concat(arr);
+}
+
+console.log(doubleArray([1, 2, 3])); // [1,2,3,1,2,3]
+
+//3. func take any arrays as input, remove first element of each and return resulted array
+function changeCollection() {
+  let res = [];
+  for (let i = 0; i < arguments.length; i++) {
+    if (Array.isArray(arguments[i])) {
+      let item = arguments[i].slice();
+      item.shift();
+      res.push(item);
+    }
+  }
+
+  return res;
+};
+
+console.log(changeCollection([1, 2, 3], ["a", "b", "c"]));
+console.log(changeCollection([1, 2, 3], [5, 6, 7]));
+
+//4. filter users by all values passed, check that parameters passed
+const myUsers = [
+  {
+    "_id": "5e36b779dc76fe3db02adc32",
+    "balance": "$1,955.65",
+    "picture": "http://placehold.it/32x32",
+    "age": 33,
+    "name": "Berg Zimmerman",
+    "gender": "male"
+  },
+  {
+    "_id": "5e36b779d117774176f90e0b",
+    "balance": "$3,776.14",
+    "picture": "http://placehold.it/32x32",
+    "age": 37,
+    "name": "Deann Winters",
+    "gender": "female"
+  },
+  {
+    "_id": "5e36b779daf6e455ec54cf45",
+    "balance": "$3,424.84",
+    "picture": "http://placehold.it/32x32",
+    "age": 36,
+    "name": "Kari Waters",
+    "gender": "female"
+  }
+];
+
+
+function filterUsers(arr, key, value) {
+  if (!arr || !key || !value) {
+    return new Error('Error message');
+  }
+
+  return arr.filter((item) => item[key] === value);
+};
+
+console.log(filterUsers(myUsers, "age", 36));
